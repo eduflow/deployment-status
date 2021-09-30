e65c94f1ccc0d3df4b74734459729ffc0ef3cb48
Tony Narlock <tony@git-pull.com>
2021-09-30 00:03:25 -0500

    Format with prettier

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 type DeploymentState =
-  | "error"
-  | "failure"
-  | "inactive"
-  | "in_progress"
-  | "queued"
-  | "pending"
-  | "success";
+  | 'error'
+  | 'failure'
+  | 'inactive'
+  | 'in_progress'
+  | 'queued'
+  | 'pending'
+  | 'success'
 
 async function run() {
   try {
-    const context = github.context;
-    const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+    const context = github.context
+    const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`
 
-    const token = core.getInput("token", { required: true });
-    const url = core.getInput("target_url", { required: false }) || defaultUrl;
-    const logUrl = core.getInput("log_url", { required: false }) || defaultUrl;
-    const description = core.getInput("description", { required: false }) || "";
-    const deploymentId = core.getInput("deployment_id");
+    const token = core.getInput('token', {required: true})
+    const url = core.getInput('target_url', {required: false}) || defaultUrl
+    const logUrl = core.getInput('log_url', {required: false}) || defaultUrl
+    const description = core.getInput('description', {required: false}) || ''
+    const deploymentId = core.getInput('deployment_id')
     const environmentUrl =
-      core.getInput("environment_url", { required: false }) || "";
-    const state = core.getInput("state") as DeploymentState;
+      core.getInput('environment_url', {required: false}) || ''
+    const state = core.getInput('state') as DeploymentState
 
-    const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
+    const client = new github.GitHub(token, {previews: ['flash', 'ant-man']})
 
     await client.repos.createDeploymentStatus({
       ...context.repo,
@@ -33,12 +33,12 @@ async function run() {
       log_url: logUrl,
       target_url: url,
       description,
-      environment_url: environmentUrl,
-    });
+      environment_url: environmentUrl
+    })
   } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+    core.error(error)
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
